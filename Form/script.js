c9e6d726591d69330cc5d07b175114752b90636b
maremoto <maregawalter86@gmail.com>
2024-11-04 10:43:12 -0300

    ultima modificacion

--- Form/script.js
+++ Form/script.js
@@ -2,13 +2,16 @@ function modificar() {
     document.getElementById("modificar").style.display = "block";
     document.getElementById("registro").style.display = "none";
     document.getElementById("consultar").style.display = "none";
+
+
 }
 
-function registro() {
-    document.getElementById("modificar").style.display = "none";
+function registrar() {
     document.getElementById("registro").style.display = "block";
+    document.getElementById("modificar").style.display = "none";
     document.getElementById("consultar").style.display = "none";
 }
+
 
 function consultar() {
     document.getElementById("modificar").style.display = "none";
